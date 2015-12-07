5cbc959312f569bee7621f43179d9fdf6ff76cb7
Ben Thomas <ben.thomas1984@gmail.com>
2015-12-07 12:10:29 +0800

    Update DiscreteIY.cxx

--- src/DiscreteIY.cxx
+++ src/DiscreteIY.cxx
@@ -36,7 +36,7 @@
 
 #include "petpvcDiscreteIYPVCImageFilter.h"
 
-const char * const VERSION_NO = "0.0.1";
+const char * const VERSION_NO = "15.1.0";
 const char * const AUTHOR = "Benjamin A. Thomas";
 const char * const APP_TITLE = "Discrete Iterative Yang (IY) PVC";
 
