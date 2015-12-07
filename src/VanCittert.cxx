2ea068e407f88e02b2170215eb5d793d0c2bae77
Ben Thomas <ben.thomas1984@gmail.com>
2015-12-07 12:05:19 +0800

    Update VanCittert.cxx
    Version no.

--- src/VanCittert.cxx
+++ src/VanCittert.cxx
@@ -33,7 +33,7 @@
 
 #include "petpvcVanCittertPVCImageFilter.h"
 
-const char * const VERSION_NO = "0.0.1";
+const char * const VERSION_NO = "15.1.0";
 const char * const AUTHOR = "Benjamin A. Thomas";
 const char * const APP_TITLE = "Reblurred Van-Cittert (RVC)";
 
